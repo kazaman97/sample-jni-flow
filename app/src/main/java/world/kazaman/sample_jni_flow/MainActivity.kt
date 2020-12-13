5f285cf9bd18ef0907757eabb13c302501d43164
kazaman97 <narusai97@gmail.com>
2020-12-13 14:53:49 +0900

    create ui

--- app/src/main/java/world/kazaman/sample_jni_flow/MainActivity.kt
+++ app/src/main/java/world/kazaman/sample_jni_flow/MainActivity.kt
@@ -1,11 +1,16 @@
 package world.kazaman.sample_jni_flow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import world.kazaman.sample_jni_flow.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+internal class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
