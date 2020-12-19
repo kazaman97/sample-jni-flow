67376648910878b69862bd915cade2b7aa9ac6ea
kazaman97 <narusai97@gmail.com>
2020-12-20 01:39:10 +0900

    fix code

--- app/src/main/java/world/kazaman/sample_jni_flow/MainActivity.kt
+++ app/src/main/java/world/kazaman/sample_jni_flow/MainActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import world.kazaman.sample_jni_flow.databinding.ActivityMainBinding
 import world.kazaman.timer_library.TimerLibrary
 
@@ -24,7 +21,7 @@ internal class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.startButton.setOnClickListener { view ->
-            lifecycleScope.launch {
+            lifecycleScope.launchWhenStarted {
                 view.isEnabled = false
                 start()
                 view.isEnabled = true
--- timer-library/src/main/cpp/timer.cpp
+++ timer-library/src/main/cpp/timer.cpp
@@ -34,7 +34,8 @@ void timer(int time) {
     while (g_running_timer) {
         auto now = std::chrono::system_clock::now();
         auto now_c = std::chrono::system_clock::to_time_t(now);
-        auto current_time = time - (now_c - started_at_c);
+        auto diff_time = now_c - started_at_c;
+        auto current_time = time - diff_time;
         if (current_time < 0) {
             g_running_timer = false;
         } else {
